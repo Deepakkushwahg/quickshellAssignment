864ef9c96ae7e0d85850aecfeed1eb5ef7fa60eb
Deepakkushwahg <deepakkushwahg@gmail.com>
2023-11-20 19:52:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Homescreen from './Components/Homescreen';
+import GroupingByStatus from './Components/GroupingByStatus';
+import GroupingByUser from './Components/GroupingByUser';
+import GroupingByPriority from './Components/GroupingByPriority';
+import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Homescreen/>} />
+        <Route exact path='/users' element={<GroupingByUser/>}/>
+        <Route exact path='/priority' element={<GroupingByPriority/>}/>
+        <Route exact path='/status' element={<GroupingByStatus/>}/>
+      </Routes>
     </div>
+    
+    
   );
 }
 
